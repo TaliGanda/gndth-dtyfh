235ed33f5e3f161945a9d1f72bba9652ec5ea4a8
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2026-06-13 12:22:47 +0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,10 +7,10 @@ sources = [
     "https://api.proxies.is/scraped?token=k3gOC3I11kU287677f5ca&timeout=5000&includeCountry=&type=all",
     "https://api.proxies.is/scraped?token=k3gOC3I11kU287677f5ca&timeout=15000&includeCountry=ID&type=all",
     "https://api.proxies.is/scraped?token=k3gOC3I11kU287677f5ca&timeout=1000&includeCountry=&type=all",
-    "https://tools.elitestress.st/api/proxy?license=23ff5205bbe84ef0e4b2ab4ed0340922&type=socks5&geo=ALL",
-    "https://tools.elitestress.st/api/proxy?license=23ff5205bbe84ef0e4b2ab4ed0340922&type=http&geo=ALL",
-    "https://tools.elitestress.st/api/proxy?license=23ff5205bbe84ef0e4b2ab4ed0340922&type=https&geo=ALL",
-    "https://tools.elitestress.st/api/proxy?license=23ff5205bbe84ef0e4b2ab4ed0340922&type=socks4&geo=ALL"
+    "https://tools.elitestress.st/api/proxy?license=e841d06798fd5ed7f460ad031e312454&type=socks5&geo=ALL",
+    "https://tools.elitestress.st/api/proxy?license=e841d06798fd5ed7f460ad031e312454&type=http&geo=ALL",
+    "https://tools.elitestress.st/api/proxy?license=e841d06798fd5ed7f460ad031e312454&type=https&geo=ALL",
+    "https://tools.elitestress.st/api/proxy?license=e841d06798fd5ed7f460ad031e312454&type=socks4&geo=ALL"
 ]
 
 MAX_CONCURRENT_REQUESTS = 200
