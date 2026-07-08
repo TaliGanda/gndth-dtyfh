1ab67e35aa5403d858ed73df11f42272f3928819
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2026-07-08 09:31:33 +0700

    Update flood.js

--- flood.js
+++ flood.js
@@ -54,7 +54,7 @@
  
  const secureContext = tls.createSecureContext(secureContextOptions);
  
- var proxyFile = "valid_http.txt";
+ var proxyFile = "proxy.txt";
  var proxies = readLines(proxyFile);
  
  const args = {
